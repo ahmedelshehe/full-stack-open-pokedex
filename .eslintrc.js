2381a81ad3ae380bdb1cebad97931a509f1aece1
Ahmed Ayman <ahmed.el-shehy@student.guc.edu.eg>
2023-04-17 20:10:16 +0200

    Minor fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,9 @@
 module.exports = {
   "env": {
-    "node": true,
+    "browser": true,
     "es6": true,
     "jest/globals": true
   },
-  
   "extends": [
     "eslint:recommended",
     "plugin:react/recommended"
