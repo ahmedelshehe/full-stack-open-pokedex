9558b568ce19b5adc8c20bdeceb4f563dee5c41b
Ahmed Ayman <ahmed.el-shehy@student.guc.edu.eg>
2023-04-18 01:57:26 +0200

    E2E Fix

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,15 @@
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:5000')
+  })
+  it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('pokemon page has the right content', function() {
+    cy.get('a').eq(3).click()
+    cy.contains('charmander')
+    cy.contains('solar power')
+  })
+
 })
